c0cbd4caeae39bd1f8cc4a4c49bad96bd68087b9
Mónica Blanco <mgblanco1001@gmail.com>
2022-07-14 11:57:24 +0200

    add

--- staff/monica-blanco/array/map.js
+++ staff/monica-blanco/array/map.js
@@ -1,8 +1,19 @@
+// function map(array, callback) {
+//     let result = [0]
+//     for(let i = 0; i < array.length;i++){
+//             result[i] = callback(array[i])     
+//         }
+//          return result
+
+// }
+
 function map(array, callback) {
-    let result = [0]
+    let results = []
     for(let i = 0; i < array.length;i++){
-            result[i] = callback(array[i])     
+        const element = array [i]
+        const result = callback(element)
+            results[i] = result    
         }
-         return result
+         return results
 
 }
--- staff/monica-blanco/array/map.test.js
+++ staff/monica-blanco/array/map.test.js
@@ -42,4 +42,25 @@ describe('Array.prototype.map', function () {
         check(newProductsList[3].price,36)
         check(newProductsList[0].price,48)  
     })
+    test('map people to strings', function() {
+        const person = [
+            {name:'Peter', surname:'Pan', age:15},
+            {name:'James', surname:'Hook', age:40},
+            {name:'Pepito', surname:'Grillo', age:50},
+            {name:'Wendy', surname:'Pan', age:14},
+            {name:'Pin', surname:'Ocho', age:8}
+        ]
+        
+        const toStrings = function(person){
+            return person.name + ' '+ person.surname + '('+ person.age + ')'
+      
+        const strings = people.map (toString)
+        }) 
+        check(strings.length, people.length)
+        check(strings[0], 'Peter Pan (15)')
+        check(strings[1], 'James Hook (40)')
+        check(strings[2], 'Pepito Grillo (50)')
+        check(strings[3], 'Wendy Pan (14)')
+        check(strings[4], 'Pin Ocho (8)')
+    })
 })
