94d70fb2f992433e8df7cd3e1f82fa4189cddebc
Mónica Blanco <mgblanco1001@gmail.com>
2022-07-15 11:35:33 +0200

    array

---
+++ staff/monica-blanco/array/at.js
@@ -0,0 +1,15 @@
+// posicion del elemento dentro del array
+//si es negativo el elemento devuelto se encuentra contando de de atrás a adelante
+
+function at(array, element) {
+    if (element > 0) {
+        for(let i=0; i< array.length;i++){
+
+        }
+
+    } else (element <= 0){
+        for(let i=1; i< array.length[i-1];i++){
+
+        }
+    }
+}
