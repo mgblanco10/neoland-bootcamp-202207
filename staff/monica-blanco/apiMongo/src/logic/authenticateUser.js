2b489647e2df4a6b1f168e8e6cb3973f16844189
Mónica Blanco <mgblanco1001@gmail.com>
2022-08-27 19:17:11 +0200

    wip

--- staff/monica-blanco/apiMongo/src/logic/authenticateUser.js
+++ staff/monica-blanco/apiMongo/src/logic/authenticateUser.js
@@ -1,12 +1,18 @@
 const { User } = require('../models')
-const { DuplicityError, SystemError } = require('../errors')
+const { DuplicityError, SystemError, AuthError } = require('../errors')
 const {validateEmail, validatePassword } = require('../validators')
-
+// darle una vuelt aún
 function autenticateUser (email, password){
     validateEmail(email)
     validatePassword(password)
 
-    return 
+    return User.find({email})
+    .then((userId) => User.find({email})
+    .catch(error => {
+        if (error.code === 11000)
+            throw new AuthError ('credentials wrong')
 
+        throw new SystemError(error.message)
+    })
 }
 module.exports = autenticateUser
