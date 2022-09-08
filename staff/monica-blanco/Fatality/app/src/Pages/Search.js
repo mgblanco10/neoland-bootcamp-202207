b045333d2b1e44b8f8017b44c4f0d9050598a2fb
Mónica Blanco <mgblanco1001@gmail.com>
2022-09-09 00:37:20 +0200

    api & app progress

--- staff/monica-blanco/Fatality/app/src/Pages/Search.js
+++ staff/monica-blanco/Fatality/app/src/Pages/Search.js
@@ -2,11 +2,75 @@ import Header from '../components/Header'
 
 export default function Search() {
   return (
-
     <div>
-  <Header/>
+<Header/>
+<div class="grid grid-flow-col grid-rows-2 grid-cols-3 gap-8">
+  <div class="transform scale-110 -rotate-6">
+  <img
+                className="object-cover w-full border md:container md:mx-auto"
+                src="https://i.pinimg.com/736x/e8/cc/9f/e8cc9f1cfd4528f96951586bb8bb6fa5--the-office-jam.jpg"
+            />
+  </div>
+  <div class="col-start-3 transform scale-75 rotate-6 translate-x-2 translate-y-15">
+  <img
+                className="object-cover w-full border"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZE2kdgDdtM5UMzw1DC9Ri0E-P4_FgbAw3w&usqp=CAU"
+            />
+  </div>
+  <div class="col-start-3 transform scale-75 rotate-6 translate-x-2 translate-y-15">
+  <img
+                className="object-cover w-full border"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZE2kdgDdtM5UMzw1DC9Ri0E-P4_FgbAw3w&usqp=CAU"
+            />
+  </div>
+  <div class="col-start-3 transform scale-75 rotate-6 translate-x-2 translate-y-15">
+  <img
+                className="object-cover w-full border"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZE2kdgDdtM5UMzw1DC9Ri0E-P4_FgbAw3w&usqp=CAU"
+            />
+  </div>
+  <div class="col-start-3 transform scale-75 rotate-6 translate-x-2 translate-y-15">
+  <img
+                className="object-cover w-full border"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZE2kdgDdtM5UMzw1DC9Ri0E-P4_FgbAw3w&usqp=CAU"
+            />
+  </div>
+  <div class="col-start-3 transform scale-75 rotate-6 translate-x-2 translate-y-15">
+  <img
+                className="object-cover w-full border"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZE2kdgDdtM5UMzw1DC9Ri0E-P4_FgbAw3w&usqp=CAU"
+            />
+  </div>
   
+  
+  <div class="transform scale-150 translate-y-11">
+  <img
+                className="object-cover w-full border"
+                src="https://ovacen.com/wp-content/uploads/2014/11/oficinas-con-arte.jpg"
+            />
+  </div>
+  
+  
+  <div class="transform translate-y-24">
+  <img
+                className="object-cover w-full border md:container md:mx-auto"
+                src="https://i.pinimg.com/736x/e8/cc/9f/e8cc9f1cfd4528f96951586bb8bb6fa5--the-office-jam.jpg"
+            />
+  </div>
+  
+  
+  
+  <div class="row-start-1 col-start-2 col-span-2 transform translate-x-20 translate-y-4">
+  <img
+                className="object-cover w-full border md:container md:mx-auto"
+                src="https://s3-eu-west-1.amazonaws.com/pa-digital-blog-pro/~paginaad/wp-content/uploads/2017/11/07174846/oficinas-mejores-mundo.jpg"
+            />
+  </div>
+</div>
+</div>
 
-    </div>
   )
 }
+    {/* <div>
+  
+</div> */}
--- staff/monica-blanco/Fatality/app/src/components/Header.js
+++ staff/monica-blanco/Fatality/app/src/components/Header.js
@@ -2,18 +2,22 @@ function Header(){
     return(
         <div className="">
         <nav className="flex justify-between items-center h16 bg-gray-100 text-blue-500 relative shadow-sm">
-            Logo
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M5.337 21.718a6.707 6.707 0 01-.533-.074.75.75 0 01-.44-1.223 3.73 3.73 0 00.814-1.686c.023-.115-.022-.317-.254-.543C3.274 16.587 2.25 14.41 2.25 12c0-5.03 4.428-9 9.75-9s9.75 3.97 9.75 9c0 5.03-4.428 9-9.75 9-.833 0-1.643-.097-2.417-.279a6.721 6.721 0 01-4.246.997z" clipRule="evenodd" />
+</svg>
+
 
             <div className="px-4 cursor-pointer md:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
 </svg>
+
 
             </div>
             <div className="pr-8 hidden md:block">
-            <button className='font-medium pr-4 text-base text-white h16 bg-blue-500 relative shadow-sm'> Search</button>
-            <button className='font-medium pr-4 text-base text-white h16 bg-blue-500 relative shadow-sm'>Settings</button>
-            <button className='font-medium pr-4 text-base text-whitw h16 bg-blue-500 relative shadow-sm'> Logout</button>
+            <button className='font-medium pr-4 text-base text-blue h16 bg-gray-100 relative shadow-sm'> Search</button>
+            <button className='font-medium pr-4 text-base text-blue h16 bg-gray-100 relative shadow-sm'>Settings</button>
+            <button className='font-medium pr-4 text-base text-blue h16 bg-gray-100 relative shadow-sm'> Logout</button>
             </div>
 
    
