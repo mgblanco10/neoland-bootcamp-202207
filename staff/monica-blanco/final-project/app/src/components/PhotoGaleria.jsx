b170de75e70b77128694c195f6225a7e9e357dd9
Mónica Blanco <mgblanco1001@gmail.com>
2022-09-13 01:32:45 +0200

    progressing on the final project, and presenting everything and then uploading information

--- staff/monica-blanco/final-project/app/src/components/PhotoGaleria.jsx
+++ staff/monica-blanco/final-project/app/src/components/PhotoGaleria.jsx
@@ -1,26 +1,58 @@
 import Card from "./Card";
+import Circle from "./Circle";
 
 export default function PhotoGaleria() {
   
   return (
-<div class="carousel mx-auto max-w-5xl px-4 sm:px-6 lg:px-12">
-  <div class="carousel-inner">
-    <div class="carousel-item mx-14 py-12">
-    <img src="https://www.wework.com/ideas/wp-content/uploads/sites/4/2022/09/Workspace-Design-WeWork-167-North-Green-Reshoot-2-1440x810.jpg" alt="office"/>
+<div class="justify-items-center bg-contain bg-center">
+ 
+    <div class="flex fex-row place-items-center max-w-lg mx-3 py-2">
+    <div class="p-1 w-full mt-1 bg-gray-200 sm:shadow-md sm::rounded-md sm:bg-gray-100 md:bg-gradient-to-r from-green-400 to-blue-500">
+      <p class="mt-2 text-gray-600">Esta caja es adaptable. Diseño primero para las dimensiones pequeñas y voy aumentando para las grandes.</p>
+      </div>
+    <img class="p-3 bg-cover" src="https://www.wework.com/ideas/wp-content/uploads/sites/4/2022/09/Workspace-Design-WeWork-167-North-Green-Reshoot-2-1440x810.jpg" alt="office"/>
+
+    <div class="flex flex-row h-90">
+    <div class=" p-16 w-60 h-65 px-3 bg-gray-200 bg-gray-100 md:bg-gradient-to-r from-green-400 to-blue-500">
+    
+      <p class="mt-3 text-gray-600">Esta caja es adaptable. Diseño primero para las dimensiones pequeñas y voy aumentando para las grandes.</p>
+      </div>
+
+
+
+     <div class="flex flex-row h-70">
+        <div className="w-60 h-60 bg-gradient-to-r from-green-400 to-blue-500 rounded-full animate-pulse m-9 p-8" />
+      </div>
+
+      </div>
+
+
+
+
+
+
 
     </div>
-    <img src="https://www.wework.com/ideas/wp-content/uploads/sites/4/2022/08/iStock-1319428315_Five-lessons-from-building-a-successful-YouTube-channel_v1-scaled.jpg" alt="office"/>
+    <div class="flex justify-center mx-auto px-3">
+    <div class="max-w-md p-1 mx-auto px-4 bg-gray-200 bg-gray-100 md:bg-gradient-to-r from-green-400 to-blue-500">
+    <img class="p-3" src="https://www.dynamobel.com/wp-content/uploads/2022/03/SH_04-1920x1053.jpg" alt="office"/>
+      <h2 class="text-lg font-semibold text-center text-blue-500 sm:text-xl">Me adapto a todo</h2>
+      <p class="mt-3 text-gray-600">Esta caja es adaptable. Diseño primero para las dimensiones pequeñas y voy aumentando para las grandes.</p>
+      </div>
+      <div class="max-w-md p-1 mx-auto px-3 bg-gray-200 bg-gray-100 md:bg-gradient-to-r from-green-400 to-blue-500">
+    <img class="p-3" src="https://www.domingoloro.com/images/portfolio/infograf%C3%ADa-3D-coworking-domingo-loro.jpg" alt="office"/>
+      <h2 class="text-lg font-semibold text-center text-blue-500 sm:text-xl">Me adapto a todo</h2>
+      <p class="mt-3 text-gray-600">Esta caja es adaptable. Diseño primero para las dimensiones pequeñas y voy aumentando para las grandes.</p>
+      </div>
+      
     </div>
     </div>
+  
 
     );
 }
 
 
-{/* <div class="max-w-md p-4 mx-auto mt-4 bg-gray-200 sm:shadow-md sm::rounded-md sm:bg-gray-100 sm:p-6 md:bg-green-100">
-  <h2 class="text-lg font-semibold text-center text-blue-500 sm:text-xl">Me adapto a todo</h2>
-  <p class="mt-3 text-gray-600">Esta caja es adaptable. Diseño primero para las dimensiones pequeñas y voy aumentando para las grandes.</p>
-</div> */}
 
 
 
